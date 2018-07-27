d44f6296812e8409afea6bdfc5f37147052baa2a
binyu_J <yubinlove1991@gmail.com>
2018-07-28 01:11:48 +0800

    get PORT from env and add time use

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var MongoClient = require('mongodb').MongoClient;
 var url = process.env.MONGODB_URL || "mongodb://$user:$pass@$host:$port/mac2corp";
 var dbName = "mac2corp";
-var serverport = 8080;
+var serverport = process.env.PORT || 8080;
 
 var HTML = 
     '<html>'
@@ -30,18 +30,18 @@ app.get('/', function (req, res) {
 
 app.post('/mac', urlencodedParser, function (req, res) {
     var mac = req.body.mac;
-
+    mac = mac.trim();
+    mac = mac.replace(/-*:*/g, '');
+    mac = mac.toUpperCase();
+    mac = mac.substring(0, 6);
+    console.log(mac);
+    var whereStr = {"company_id": mac};  // 查询条件
+    var start = new Date();
     MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
         if (err) throw err;
-        var mac = req.body.mac;
-        mac = mac.trim();
-        mac = mac.replace(/-*:*/g, '');
-        mac = mac.toUpperCase();
-        mac = mac.substring(0, 6);
-        console.log(mac);
-        var whereStr = {"company_id": mac};  // 查询条件
-        db.db().collection("oui").find(whereStr, { _id: 0}).toArray(function(err, result) {
+        db.db(dbName).collection("oui").find(whereStr, { _id: 0}).toArray(function(err, result) {
             if (err) throw err;
+            res.append('Timeuse', new Date()-start);
             res.json(JSON.stringify(result));
         });
         db.close();
